83bb49bf928af91b4d37d7017572589c228c3e1f
David Alexander <dga.1984@gmail.com>
2023-10-20 23:39:50 +0100

    update

--- scripts/mask_var.py
+++ scripts/mask_var.py
@@ -1,19 +1,29 @@
-import rasterio
+import os
 from osdatahub import Extent
 from osdatahub import NGD
-from osdatahub import Extent
 import geopandas as gpd
+import rasterio
 
-def bb(tif):
-    t = rasterio.open(tif)
-    b = Extent.from_bbox(t.bounds, crs="EPSG:27700")
-    return b
+def bb(lyr):
+    if isinstance (lyr, rasterio.io.DatasetReader):
+        t = Extent.from_bbox(lyr.bounds, crs="EPSG:27700")
+        return t
+    else:
+        g = round(lyr.bounds)
+        return g # note this only works with pd
 
 def urban_mask(gdf):
     m = gpd.GeoDataFrame.from_features(gdf, crs="EPSG:27700")
+    # subset based on made surface and garden for proper urban
     m = (m.loc[(m['description']== 'Made Surface') | (m['description']== 'Residential Garden')])
     m.unary_union
     m['pdnp'] = 'pdnp'
     m = m.dissolve(by = 'pdnp', as_index=False)
     m = m.buffer(10)
     return m
+
+def wall_mask(gdf):
+    walls = gpd.GeoDataFrame.from_features(gdf, crs="EPSG:27700")
+    #subset based on 'Built Obstruction'
+    walls = walls[walls['description'] == 'Built Obstruction']
+    return walls
