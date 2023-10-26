fb30365d9e05f9bc9b0c4e19bfc80fb7652f5e5f
David Alexander <dga.1984@gmail.com>
2023-10-26 15:27:04 +0100

    fix to subset

--- scripts/mask_var.py
+++ scripts/mask_var.py
@@ -1,6 +1,7 @@
 import os
 from osdatahub import Extent
 import geopandas as gpd
+import pandas as pd
 import rasterio
 
 def bb(lyr): # creates a BBox of input layers
@@ -11,10 +12,16 @@ def bb(lyr): # creates a BBox of input layers
         g = round(lyr.bounds)
         return g # note this only works with pd
     
+def sub(d): # subset the land df with made surfaces or gardens
+    db = (d.loc[(d['theme'] == 'Land')]) # subset land
+    db = (db.loc[(db['description'] == 'Made Surface') | (db['description'] == 'Residential Garden')]) # subset the subset of land
+    d = (d.loc[(d['theme'] != 'Land')])   # drop the land from the origianl df
+    d = pd.concat([d, db])
+    return d
+    
 def urban_mask(gdf): # creates an urban mask of villages from os api
     m = gpd.GeoDataFrame.from_features(gdf, crs="EPSG:27700")
-    # subset based on made surface and garden for proper urban
-    m = (m.loc[(m['description']== 'Made Surface') | (m['description']== 'Residential Garden')])
+    m = sub(m)
     m.unary_union
     m['pdnp'] = 'pdnp'
     m = m.dissolve(by = 'pdnp', as_index=False)
