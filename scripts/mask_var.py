35161295e165d4068f18b4167b311b6446abe578
David Alexander <dga.1984@gmail.com>
2023-12-13 17:41:57 +0000

    Update

--- scripts/mask_var.py
+++ scripts/mask_var.py
@@ -6,13 +6,18 @@ import pandas as pd
 import numpy as np
 import rasterio
 
+'''
+Subset OS API
+
+'''
+
 def bb(lyr): # creates a BBOX of input layers
     if isinstance (lyr, rasterio.io.DatasetReader):
-        t = Extent.from_bbox(lyr.bounds, crs="EPSG:27700")
-        return t
+        lyr = Extent.from_bbox(lyr.bounds, crs="EPSG:27700")
+        return lyr
     else:
-        g = round(lyr.bounds)
-        return g # note this only works with pd
+        lyr = round(lyr.bounds)
+        return lyr # note this only works with pd
     
 def sub(d): # subset the land df with made surfaces or gardens
     db = (d.loc[(d['theme'] == 'Land')]) # subset land
@@ -26,20 +31,23 @@ def sub(d): # subset the land df with made surfaces or gardens
     return d
     
 def urban_mask(gdf): # creates an urban mask of villages from os api
-    m = gpd.GeoDataFrame.from_features(gdf, crs="EPSG:27700")
-    m = sub(m)
-    m.unary_union
-    m['pdnp'] = 'pdnp'
-    m = m.dissolve(by = 'pdnp', as_index=False)
-    m = m.buffer(10)
-    m = gpd.GeoDataFrame.from_features(m, crs ="EPSG:27700")
-    return m
+    gdf = gpd.GeoDataFrame.from_features(gdf, crs="EPSG:27700")
+    gdf = sub(gdf)
+    gdf.unary_union
+    gdf['pdnp'] = 'pdnp'
+    gdf = gdf.dissolve(by = 'pdnp', as_index=False)
+    gdf = gdf.buffer(10)
+    gdf = gpd.GeoDataFrame.from_features(gdf, crs ="EPSG:27700")
+    return gdf
 
 def wall_subset(gdf): # takes the subset based on built obstructions
     w = gpd.GeoDataFrame.from_features(gdf, crs="EPSG:27700")
     #subset based on 'Built Obstruction'
     w = (w.loc[w['description'] == 'Built Obstruction'])
     return w
+
+    
+
 
 '''
 Turn gdf of shape file polygon into a raster file. Possibly store & plot.
@@ -70,14 +78,14 @@ def convert_shp_mask_to_raster(df_shp, col_name='theme',
     if col_name in df_shp.columns and col_name not in cube.data_vars:
         print(cube)
     shape_cube = cube[col_name].shape  # somehow sometimes an extra row or of NO CLASS is added... 
-    if shape_cube[0]  == 7449:
+    if shape_cube[0]  == 1491:
         if len(np.unique(cube[col_name][0, :])) > 1:
             print(f'WARNING: {filename} has shape {shape_cube} but first y-row contains following classes: {np.unique(cube[col_name][:, 0])}. Still proceeding..')    
-        cube = cube.isel(y=np.arange(1, 7449))  #discard first one that is just no classes 
-    if shape_cube[1] == 7449:
+        cube = cube.isel(y=np.arange(1, 1491))  #discard first one that is just no classes 
+    if shape_cube[1] == 1490:
         if len(np.unique(cube[col_name][:, 0])) > 1:
             print(f'WARNING: {filename} has shape {shape_cube} but first x-col contains following classes: {np.unique(cube[col_name][:, 0])}. Still proceeding..')    
-        cube = cube.isel(x=np.arange(1, 7449))  #discard first one that is just no classes 
+        cube = cube.isel(x=np.arange(1, 1490))  #discard first one that is just no classes 
 
     ## Decrease data size:
     if verbose > 0:
